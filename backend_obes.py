81ea5ee3bd7fbcb0d60e631880911d1293c4f213
christoperBar <cbarnaby@student.ciputra.ac.id>
2024-12-23 18:00:39 +0700

    first commit

---
+++ backend_obes.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+import pickle
+
+# Load model
+with open("svc_model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+app = Flask(__name__)
+CORS(app)  
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.json  # Data dari front-end
+    input_data = data["features"]
+    prediction = model.predict([input_data])
+    return jsonify({"prediction": int(prediction[0])})
+
+if __name__ == "__main__":
+    app.run(debug=True)
